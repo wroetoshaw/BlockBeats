c75a276fc099f797f4a9a6cef84acfd0448e36c3
milind_kunale <milind.kunale2019@vitstudent.ac.in>
2023-03-25 14:50:47 +0530

    version 2.1.2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "368feb284457d142"; }
+/******/ 		__webpack_require__.h = function() { return "bb96b503fed00afc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/index.js
+++ components/index.js
@@ -8,5 +8,6 @@ import Input from './Input';
 import Loader from './Loader';
 import Modal from './Modal';
 import SearchBar from './SearchBar';
+import Hero from './Hero';
 
-export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard, Loader, Modal, Input, SearchBar };
+export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard, Loader, Modal, Input, SearchBar, Hero };
